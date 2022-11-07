e35587ad077c335f3a0eeb102ef75fda40e89119
Tiago <ztq7600@gmail.com>
2022-11-07 15:29:00 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    let textNum = 0;
+    setInterval(function() {
+        switch(textNum) {
+            case 0:
+                $("#text").html('Fresh Roasted Coffee');
+                textNum = 1;
+                break;
+            case 1:
+                $('#text').html('Scones & Bagels');
+                textNum = 2;
+                break;
+            case 2:
+                $('#text').html('Los Angeles Tourist District');
+                textNum = 0;
+                break;
+        }
+    }, 1000)
+
+    
+
+
+
+    let backgroundNum = 0;
+    setInterval(function() {
+        switch (backgroundNum) {
+            case 0:
+                $('body').css('background-image', 'url(/pics/background-2.jpg');
+                backgroundNum = 1;
+                break;
+                case 1:
+                    $('body').css('background-image', 'url(/pics/background-3.jpg');
+                    backgroundNum = 2;
+                    break;
+                    case 2:
+                        $('body').css('background-image', 'url(/pics/background-1.jpg');
+                        backgroundNum = 0;
+                        break;
+        }
+    }, 2000)
+})
